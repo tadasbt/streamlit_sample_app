0a5907ef4a335d936633b88fc807caf9a32971e5
tadasbt <152064624+tadasbt@users.noreply.github.com>
2026-01-07 08:28:18 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,19 @@
 import requests
+from bs4 import BeautifulSoup
+import re
 
+BASE_URL = "https://s.minkabu.jp/stock/
+{code}/dividend"
+HEADERS = {
+  "User-Agent":"Mozilla/5.0"
+}
+
+def get_minkabu_dividend_month(code:str) -> list[str] | None:
+  """
+  例： code='7272' -> ['6月','12月'] を返す想定。
+  取得できなければ None。
+  """
+  url = BASE_URL.format(code=code)
 x=10
 'x: ', x
 'import requests'
